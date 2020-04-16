30a92aae15d4338f0f5c34f62c221032a9a7c27e
lzhhuo <1007320404@qq.com>
2020-04-16 23:17:38 +0800

    03课后作业

---
+++ src/leetcode/editor/cn/P21MergeTwoSortedLists.java
@@ -0,0 +1,61 @@
+//将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 示例： 
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+// 
+// Related Topics 链表
+
+package leetcode.editor.cn;
+
+//java:合并两个有序链表
+public class P21MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new P21MergeTwoSortedLists().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            } else if (l2 == null) {
+                return l1;
+            } else if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode prehead = new ListNode(-1);
+            ListNode prev = prehead;
+            while (l1 != null && l2 != null) {
+                if (l1.val <= l2.val) {
+                    prev.next = l1;
+                    l1 = l1.next;
+                } else {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+                prev = prev.next;
+            }
+            prev.next = l1 == null ? l2 : l1;
+            return prehead.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
