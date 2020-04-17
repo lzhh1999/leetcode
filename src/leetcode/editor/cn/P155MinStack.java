1900f81c4a0db22f6b58e0cc20b4baeb4b0cf1c7
lzhhuo <1007320404@qq.com>
2020-04-17 14:03:04 +0800

    04有效括号、最小栈等问题

---
+++ src/leetcode/editor/cn/P155MinStack.java
@@ -0,0 +1,81 @@
+//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。 
+//
+// 
+// push(x) —— 将元素 x 推入栈中。 
+// pop() —— 删除栈顶的元素。 
+// top() —— 获取栈顶元素。 
+// getMin() —— 检索栈中的最小元素。 
+// 
+//
+// 示例: 
+//
+// MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.getMin();   --> 返回 -3.
+//minStack.pop();
+//minStack.top();      --> 返回 0.
+//minStack.getMin();   --> 返回 -2.
+// 
+// Related Topics 栈 设计
+
+package leetcode.editor.cn;
+
+import java.util.Stack;
+
+//java:最小栈
+public class P155MinStack{
+    public static void main(String[] args){
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if (!minStack.isEmpty()) {
+            int top = minStack.peek();
+            if (x <= top) {
+                minStack.push(x);
+            }
+        }else{
+            minStack.push(x);
+        }
+    }
+    
+    public void pop() {
+        int pop = stack.pop();
+        int top = minStack.peek();
+        if (pop == top) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/P239SlidingWindowMaximum.java
@@ -0,0 +1,67 @@
+//给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。 
+//
+//
+// 返回滑动窗口中的最大值。 
+//
+// 
+//
+// 进阶： 
+//
+// 你能在线性时间复杂度内解决此题吗？ 
+//
+// 
+//
+// 示例: 
+//
+// 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+//输出: [3,3,5,5,6,7] 
+//解释: 
+//
+//  滑动窗口的位置                最大值
+//---------------               -----
+//[1  3  -1] -3  5  3  6  7       3
+// 1 [3  -1  -3] 5  3  6  7       3
+// 1  3 [-1  -3  5] 3  6  7       5
+// 1  3  -1 [-3  5  3] 6  7       5
+// 1  3  -1  -3 [5  3  6] 7       6
+// 1  3  -1  -3  5 [3  6  7]      7 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10^5 
+// -10^4 <= nums[i] <= 10^4 
+// 1 <= k <= nums.length 
+// 
+// Related Topics 堆 Sliding Window
+
+package leetcode.editor.cn;
+
+//java:滑动窗口最大值
+public class P239SlidingWindowMaximum{
+    public static void main(String[] args){
+        Solution solution = new P239SlidingWindowMaximum().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        if (len * k == 0) {
+            return new int[0];
+        }
+        int[] output = new int[len - k + 1];
+        for (int i = 0; i < len - k + 1; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = i; j < k + i; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            output[i] = max;
+        }
+        return output;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
